803984945d6f166a9b479a1a3fb54fdd7efba11f
Thadeu Lima de Souza Cascardo <cascardo@cascardo.info>
2013-06-12 08:40:58 -0300

    Add more fields to the first message.
    The only missing fields are the size and hash of the second message and
    part of the header register.

--- rnet_encode.c
+++ rnet_encode.c
@@ -74,6 +74,9 @@ int rnet_encode(struct rnet_decfile *decfile, struct rnet_message **msg)
 	r = rnet_message_add_ascii(*msg, "origem", "JA2R");
 	r = rnet_message_add_ascii(*msg, "so", "GNU");
 	r = rnet_message_add_ascii(*msg, "cliente", "201104");
+	r = rnet_message_add_u32(*msg, "tam_dados_val", 0);
+	r = rnet_message_add_u64(*msg, "tam_dados_val_chave", 0);
+	r = rnet_message_add_u32(*msg, "arquivos_restantes", 0);
 
 	if (r < 0)
 		return r;
